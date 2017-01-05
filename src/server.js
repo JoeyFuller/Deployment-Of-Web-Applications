1aa5b71e78e6945c37dca91c2cce0005cf982022
joey <joeyfuller@fullsail.edu>
2017-01-05 01:14:48 -0500

    added basic api and route test

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ var port = 3000; //port server is running
 
 app.use('/api', require('../routes/api.js')(express));
 
-app.listen(port, function(){
+var server = app.listen(port, function(){
 	console.log('Server Active on', port); //console log to show server active
 });
+
+module.exports = server;
--- test/_api.js
+++ test/_api.js
@@ -1,8 +1,32 @@
-var expect = require('expect'); //unit test
+var server = require('../src/server.js');
+var request = require('supertest');
 
 describe('API', function(){
-	it('Should probably load, somehow.', function(){
+	var server;
+	beforeEach(function(){
+		server = require('../src/server.js');
+
+	});
+
+	afterEach(function(){
+		server.close();
+	});
+
+	it('Should return specified object', function testHealth(done){
+		request(server)
+			.get('/api/')
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200, { hello: "world"}, done);
 		
-		expect(100).toBe(100)
+	});
+
+	it('/status should return specified healthy:true.', function testHealth(done){
+		request(server)
+			.get('/api/status')
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200, { healthy: true}, done);
+		
 	});
 });
